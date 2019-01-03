9256e5188fadaa5c4ca0b3a0954533e4944f8284
Nitin <Nitin@Exts-Rent-13.fswwindia.com>
2019-01-03 14:56:16 +0530

    Re adding the Arraylist

---
+++ JavaExamples/src/Collections/Examples/Arraylist.java
@@ -0,0 +1,19 @@
+package Collections.Examples;
+
+import java.util.ArrayList;
+
+public class Arraylist {
+
+	public static void main(String[] args) {
+		ArrayList<String> list=new ArrayList<String>();//Creating arraylist    
+	      list.add("Ravi");//Adding object in arraylist    
+	      list.add("Vijay");    
+	      list.add("Ravi");    
+	      list.add("Ajay");    
+	      //Invoking arraylist object   
+	      System.out.println(list);  
+	  }  
+	 
+	
+
+}

4316227143c350c740eafc2fa6cf03c55cd88218
Nitin <Nitin@Exts-Rent-13.fswwindia.com>
2019-01-03 15:16:07 +0530

    tezs

--- JavaExamples/src/Collections/Examples/test.java
+++ JavaExamples/src/Collections/Examples/test.java
@@ -3,7 +3,7 @@ package Collections.Examples;
 public abstract class test {
 
 	public static void main(String[] args) {
-	System.out.println("working on the scenarios");
+	System.out.println("working on the scenarios o the asdgarg");
 
 	}
 

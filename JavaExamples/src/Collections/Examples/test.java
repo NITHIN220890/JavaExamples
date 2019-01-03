9b7d81b2c7b551067fa40213f819900889f70fe1
Nitin <Nitin@Exts-Rent-13.fswwindia.com>
2019-01-03 15:14:12 +0530

    adding in the test class along

--- JavaExamples/src/Collections/Examples/test.java
+++ JavaExamples/src/Collections/Examples/test.java
@@ -3,7 +3,7 @@ package Collections.Examples;
 public abstract class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("working on the scenarios");
 
 	}
 
